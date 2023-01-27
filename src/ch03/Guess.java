2669c3a06aa7dff1c63bc69251a754b216bad18e
Jason D'Oyley <jdfdoyley@gmail.com>
2023-01-27 07:46:37 -0500

    Create a guess the letter game

---
+++ src/ch03/Guess.java
@@ -0,0 +1,21 @@
+package ch03;
+
+/**
+ * Author: Jason D'Oyley
+ * Date: Jan 27, 2023
+ *
+ * Guess the letter game.
+ */
+class Guess {
+    public static void main(String[] args) throws java.io.IOException {
+        char ch, answer = 'K';
+
+        System.out.println("I'm thinking of a letter between A and Z.");
+        System.out.print("Can you guess it: ");
+
+        ch = (char) System.in.read();   // read a char from the keyboard
+
+        if (ch == answer)
+            System.out.println("** Right **");
+    }
+}
